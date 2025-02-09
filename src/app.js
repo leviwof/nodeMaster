d4189a2ad6bf82791db41874bf867ed3e5a7a994
leviwof <ganeshkemoap123@gmail.com>
2025-02-09 18:44:45 +0530

    added a server

--- src/app.js
+++ src/app.js
@@ -1,1 +1,21 @@
 console.log("First push done");
+
+//express server required using required
+const express = require("express");
+//using express server
+const app = express();
+
+//listening the app server on port 
+app.listen(9999, () => {
+    console.log("Server is running on port 9999");
+})
+
+//demo request handler
+app.use("/demo1", (req, res) => {
+    res.send("Namaste Duniya bhai");
+})
+
+//demo request handler
+app.use("/demo2", (req, res) => {
+    res.send("hello jo");
+})
